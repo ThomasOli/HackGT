a0d96fff2997cda4ba0aa0510d3b87dcd105b235
Shane Downs <115857643+shane-downs@users.noreply.github.com>
2023-10-13 22:40:40 -0400

    Update mongo

--- mongo/data.py
+++ mongo/data.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import datetime
 
 uri = "mongodb+srv://gators:G8t0rs@cluster0.tt5rvec.mongodb.net/?retryWrites=true&w=majority"
 
@@ -11,8 +12,8 @@ db = client.mydatabase
 collection = db.mycollection
 
 documents = [
-    {"name": "Alice", "age": 25},
-    {"name": "Bob", "age": 35},
+    {"name": "Alice", "access_time": "10/13/2023"},
+    {"name": "Bob", "access_time": "10/17/2023"},
 ]
 
 collection.insert_many(documents)
