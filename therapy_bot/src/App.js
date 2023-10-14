f5310ed11c189c0bcd188b183d11beebca81974e
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-10-14 02:16:22 -0400

    navbar changes

--- therapy_bot/src/App.js
+++ therapy_bot/src/App.js
@@ -4,9 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import Navbar from './Components/nav';
 
+const BlueBorder = () => {
+  return <div style={{ backgroundColor: 'blue', height: '5px', width: '100%' }}></div>;
+};
+
 function App() {
   return (
     <div>
+      <BlueBorder />
       <Router>
           <Navbar />
         <Routes>
