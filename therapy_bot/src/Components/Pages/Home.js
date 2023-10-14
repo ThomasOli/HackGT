0ebb28ffd1a491d4dfcc83c96fb920fc79beb2e9
Ananya Sista <sistaa@ufl.edu>
2023-10-14 03:36:53 -0400

    added parallax scrolling effect

--- therapy_bot/src/Components/Pages/Home.js
+++ therapy_bot/src/Components/Pages/Home.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Parallax, ParallaxLayer } from 'react-scroll-parallax';
-import Gator from "../../images/gator.png";
+import { Parallax, ParallaxLayer, ParallaxProvider } from 'react-scroll-parallax';
+import Gator from "../../images/sparkle.jpg";
 import './Home.css';
 
 function Home() {
     return (
-        <div className='App'>
-            <Parallax strength={600} bgImage={Gator}>
-                <div className='content'>
-                    <div className='text-content'>Normal Parallax</div>
-                </div>
-            </Parallax>
-            <div className='content'></div>
+        // <ParallaxProvider>
+        // <div className='App'>
+        //     <Parallax strength={600} bgImage={Gator}>
+        //         <div className='content'>
+        //             <div className='text-content'>askdfj Parallax</div>
+        //         </div>
+        //     </Parallax>
+        //     <div className='content'></div>
+        // </div>
+        // </ParallaxProvider>
+        <div>
+        <div class="parallax"></div>
+
+        <div className='content'>
+        Scroll Up and Down this page to see the parallax scrolling effect.
+        This div is just here to enable scrolling.
+        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+        </div>
+
+        <div class="parallax2"></div>
         </div>
     )
 }
