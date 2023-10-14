c4a93f83efd1cdabe7ac639fe077a0d09b03b7d8
Ananya Sista <sistaa@ufl.edu>
2023-10-14 19:36:49 -0400

    set up topics system

--- therapy_bot/src/Components/journals.js
+++ therapy_bot/src/Components/journals.js
@@ -17,21 +17,44 @@ import Book from './Images/book.png';
 import { Block } from '@mui/icons-material';
 import "./journals.css"
 import SummarizeIcon from '@mui/icons-material/Summarize';
+import {useState} from 'react';
 
 
 
 const emails = ['Calculus', 'Economics', 'Research Paper', 'Programming'];
 
+const content = [
+{
+  name: 'Calculus',
+  summary: 'A derivative is much easier to take than an integral in my opinion.',
+  points: 'Know how to take derivatives\nKnow how to take integrals\nKnow that one is better than the other',
+  question: 'Why are you still doing math by hand?',
+},
+{
+  name: 'Economics',
+  summary: 'It\'s actually not about money, but rather choices and the economy as a whole.',
+  points: 'Know supply\nKnow demand\nKnow that is basically everything',
+  question: 'Would you use the word surplus in a real sentence?'
+}
+]
+
+
 function SimpleDialog(props) {
   const { onClose, selectedValue, open } = props;
-
+  const [selectedSummary, setSelectedSummary] = useState('');
+  const [selectedPoints, setSelectedPoints] = useState('');
+  const [selectedQuestion, setSelectedQuestion] = useState('');
   const handleClose = () => {
     onClose(selectedValue);
   };
 
-  // const handleListItemClick = (value) => {
-  //   onClose(value);
-  // };
+  const handleListItemClick = (content) => {
+    setSelectedSummary(content.summary);
+    setSelectedPoints(content.points);
+    setSelectedQuestion(content.question);
+  };
+
+
 
   return (
     <Dialog onClose={handleClose} open={open} fullWidth maxWidth='xlg' style={{display: 'block'}}>
@@ -40,23 +63,22 @@ function SimpleDialog(props) {
           <h1>Topics:</h1>
         </div>
         <div className='halfBox'>
-          <h1>Topics:</h1>
+          <h1>Notes:</h1>
         </div>
       </div>
-      <div>  
+      <div style={{marginLeft: '40px'}}>  
         <div className="halfBox">
             
             <List sx={{ pt: 0 }}>
-            {emails.map((email) => (
-            <ListItem disableGutters key={email}>
-            {/* <ListItemButton onClick={() => handleListItemClick(email)}> */}
-            <ListItemButton>
+            {content.map((content) => (
+            <ListItem disableGutters key={content.name}>
+            <ListItemButton onClick={() => handleListItemClick(content)}>
               <ListItemAvatar>
                 <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
                   <SummarizeIcon />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText primary={email} />
+              <ListItemText primary={content.name} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -76,7 +98,10 @@ function SimpleDialog(props) {
             </List>
         </div>
         <div className="halfBox">
-          
+          <h2>{selectedSummary}</h2>
+          <h3>{selectedPoints}</h3>
+          <h4>{selectedQuestion}</h4>
+
         </div>
       </div>
     </Dialog>
