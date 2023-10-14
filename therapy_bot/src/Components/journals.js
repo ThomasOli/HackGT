7a78e8e7462ab33ec2936365887533cf7af5df10
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-10-14 11:36:47 -0400

    journal's page

--- therapy_bot/src/Components/journals.js
+++ therapy_bot/src/Components/journals.js
@@ -1,35 +1,136 @@
-import React from 'react';
-import './journals.css';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
 
-const journals = () => {
-    return (
-        // <div className='table'>
-        //     <table>
-        //         <tr>
-        //             <th>Name</th>
-        //             <th>Age</th>
-        //             <th>Response</th>
-        //         </tr>
-        //         <tr>
-        //             <td>Jamie</td>
-        //             <td>19</td>
-        //             <td>I am sad</td>
-        //         </tr>
-        //         <tr>
-        //             <td>Craig</td>
-        //             <td>19</td>
-        //             <td>Draw using leap</td>
-        //         </tr>
-        //         <tr>
-        //             <td>Bill</td>
-        //             <td>25</td>
-        //             <td>Hello world</td>
-        //         </tr>
-        //     </table>
-        // </div>
+const emails = ['username@gmail.com', 'user02@gmail.com'];
 
-        
-    );
+function SimpleDialog(props) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        {emails.map((email) => (
+          <ListItem disableGutters key={email}>
+            <ListItemButton onClick={() => handleListItemClick(email)}>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                  <PersonIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={email} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        <ListItem disableGutters>
+          <ListItemButton
+            autoFocus
+            onClick={() => handleListItemClick('addAccount')}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <AddIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Add account" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Dialog>
+  );
 }
 
-export default journals;
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
+export default function SimpleDialogDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
+  return (
+    <div>
+      <Typography variant="subtitle1" component="div">
+        Selected: {selectedValue}
+      </Typography>
+      <br />
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open simple dialog
+      </Button>
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </div>
+  );
+}
+
+
+// import React from 'react';
+// import './journals.css';
+
+// const journals = () => {
+//     return (
+//         // <div className='table'>
+//         //     <table>
+//         //         <tr>
+//         //             <th>Name</th>
+//         //             <th>Age</th>
+//         //             <th>Response</th>
+//         //         </tr>
+//         //         <tr>
+//         //             <td>Jamie</td>
+//         //             <td>19</td>
+//         //             <td>I am sad</td>
+//         //         </tr>
+//         //         <tr>
+//         //             <td>Craig</td>
+//         //             <td>19</td>
+//         //             <td>Draw using leap</td>
+//         //         </tr>
+//         //         <tr>
+//         //             <td>Bill</td>
+//         //             <td>25</td>
+//         //             <td>Hello world</td>
+//         //         </tr>
+//         //     </table>
+//         // </div>
+
+        
+//     );
+// }
+
+// export default journals;
