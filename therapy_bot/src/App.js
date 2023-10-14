2b0f285d787cc7bd09ccde11bafa55c9ace8da93
Shane Downs <115857643+shane-downs@users.noreply.github.com>
2023-10-14 02:08:52 -0400

    Mongo get data

--- therapy_bot/src/App.js
+++ therapy_bot/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import Navbar from './Components/nav';
-import Auth from './auth';
+import Journals from './Components/journals';
 
 function App() {
   return (
       <Router>
           <Navbar />
         <Routes>
-          <Route path = "/" element={<Home/>}></Route>
+          <Route path = "/home" element={<Home/>}></Route>
+          <Route path = "/journals" element={<Journals/>}></Route>
         </Routes>
       </Router>
   );
