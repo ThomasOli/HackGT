3e19eaca61b3fc381cc3c9cee0dd4ab6c6ff029c
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-10-14 03:17:01 -0400

    m

--- therapy_bot/src/Components/Pages/Home.js
+++ therapy_bot/src/Components/Pages/Home.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Parallax, ParallaxLayer } from 'react-scroll-parallax';
+import Gator from "../../images/gator.png";
+import './Home.css';
 
-
-const Home = () => {
+function Home() {
     return (
-        <div>
-            <p>HelloWorld</p>
+        <div className='App'>
+            <Parallax strength={600} bgImage={Gator}>
+                <div className='content'>
+                    <div className='text-content'>Normal Parallax</div>
+                </div>
+            </Parallax>
+            <div className='content'></div>
         </div>
-    );
-};
+    )
+}
+
+
+// function Component() {
+//     const parallax = useParallax({
+//       speed: -10,
+//     });
+//     return <div ref={parallax.ref} />;
+//   }
+
+// const Component = () => (
+//     <Parallax translateY={[-20, 20]}>
+//       <div className="my-thing" />
+//     </Parallax>
+//   );
+
+// function Home() {
+//     {
+//         return (
+
+//           <ParallaxProvider>
+//             <p>
+//                 helloworld
+//             </p>
+//           </ParallaxProvider>
+//         );
+//       }
+// }
 
 export default Home
