5251b02a9d88e6da33c16668861a09daffdbd8cc
Ananya Sista <sistaa@ufl.edu>
2023-10-14 11:30:38 -0400

    homepage graphcis

--- therapy_bot/src/Components/Pages/Home.js
+++ therapy_bot/src/Components/Pages/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 //import { ParallaxProvider } from "react-scroll-parallax";
 import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
-
+// import { Parallax } from "react-scroll-parallax";
 // mountain functionality from: https://github.com/jscottsmith/react-scroll-parallax-examples
 
-import bg1 from "../Images/bg-1.jpg";
+import bg1 from "../Images/bg-1.png";
 import bg2 from "../Images/bg-2.png";
 import bg3 from "../Images/bg-3.png";
 import bg4 from "../Images/bg-4.png";
@@ -14,48 +14,51 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <main>
+    <main>  
       <ParallaxProvider>
+      
         <header>
           <ParallaxBanner
             className="banner"
             layers={[
-              {
-            //     image: bg1,
-            //     translateY: [0, 50],
-            //     shouldAlwaysCompleteAnimation: true,
-            //     expanded: false,
-            //   },
-            //   {
-                image: bg2,
-                translateY: [5, 45],
-
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-              {
-                image: bg3,
-                translateY: [10, 30],
-
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-              {
-                image: bg4,
-                translateY: [15, 25],
-
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
-              {
-                image: bg5,
-                translateY: [20, 20],
-
-                shouldAlwaysCompleteAnimation: true,
-                expanded: false,
-              },
+            {
+                speed: -500,
+                children: (
+                    <div className="textdiv">
+                        <h1 className="text">HelloWOrld</h1>
+                    </div>
+                ),
+            },
+            {
+                translateY: ['-2500px', '-25px'],
+                children: (
+                    <div className="textdiv">
+                        {/* <h1 className="text">HelloWOrld</h1> */}
+                        <img src={bg1} />
+                    </div>
+                ),
+            },
+            {
+                image: bg2, speed: -10
+            },
+            {
+                image: bg3, speed: -20
+            },
+            {
+                image: bg4, speed: -30
+            },
+            {
+                image: bg5, speed: -40, 
+                
+            },
+                
             ]}
-          />
+          >
+        
+            {/* <div className="textdiv">
+                <h1 className="text">HelloWOrld</h1>
+            </div> */}
+        </ParallaxBanner>
         </header>
       </ParallaxProvider>
       <div>
