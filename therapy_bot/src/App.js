d8ed4dc4cc7ad419ded39dc019898f0ee973d091
Ananya Sista <sistaa@ufl.edu>
2023-10-14 02:15:49 -0400

    fix routes

--- therapy_bot/src/App.js
+++ therapy_bot/src/App.js
@@ -3,31 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import Navbar from './Components/nav';
-<<<<<<< HEAD
 import Login from './login';
-
-=======
 import Journals from './Components/journals';
->>>>>>> 2b0f285d787cc7bd09ccde11bafa55c9ace8da93
 
 function App() {
   return (
       <Router>
           <Navbar />
         <Routes>
-<<<<<<< HEAD
-          {/* <Route path = "/" element={<Home/>}></Route> */}
-          <Route path = "/" element={<Login/>}></Route>
+          <Route path = "/" element={<Home/>}></Route>
+          <Route path = "/journal" element={<Journals/>}></Route>
         </Routes>
       </Router>
-
-    </div>
-=======
-          <Route path = "/home" element={<Home/>}></Route>
-          <Route path = "/journals" element={<Journals/>}></Route>
-        </Routes>
-      </Router>
->>>>>>> 2b0f285d787cc7bd09ccde11bafa55c9ace8da93
   );
 }
 
