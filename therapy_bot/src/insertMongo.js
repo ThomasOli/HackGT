b339163247343397564d10ed4c7fd753895995e6
Ananya Sista <sistaa@ufl.edu>
2023-10-14 13:55:55 -0400

    commented out insertMongo

--- therapy_bot/src/insertMongo.js
+++ therapy_bot/src/insertMongo.js
@@ -1,38 +1,38 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
-async function run(email, password) {
-  const uri =
-  "mongodb+srv://gators:G8t0rs@cluster0.tt5rvec.mongodb.net/?retryWrites=true&w=majority";
+// async function run(email, password) {
+//   const uri =
+//   "mongodb+srv://gators:G8t0rs@cluster0.tt5rvec.mongodb.net/?retryWrites=true&w=majority";
 
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+//   const client = new MongoClient(uri, {
+//     serverApi: {
+//       version: ServerApiVersion.v1,
+//       strict: true,
+//       deprecationErrors: true,
+//     }
+//   });
 
-  await client.connect();
+//   await client.connect();
 
-  const dbName = "HackGTData";
-  const collectionName = "userData";
+//   const dbName = "HackGTData";
+//   const collectionName = "userData";
 
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
+//   const database = client.db(dbName);
+//   const collection = database.collection(collectionName);
 
-  const document =
-    {
-      email: email,
-      password: password
-    };
+//   const document =
+//     {
+//       email: email,
+//       password: password
+//     };
 
-  try {
-    const insertOneResult = await collection.insertOne(document);
-    console.log(`${insertOneResult.insertedCount} document successfully inserted.\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
-  }
+//   try {
+//     const insertOneResult = await collection.insertOne(document);
+//     console.log(`${insertOneResult.insertedCount} document successfully inserted.\n`);
+//   } catch (err) {
+//     console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+//   }
   
-}
+// }
 
-export default run;
+// export default run;
