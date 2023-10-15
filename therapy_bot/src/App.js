cb8f337e1e23d9abcd5460e5981dba578141d6a9
ThomasOli <51518411+ThomasOli@users.noreply.github.com>
2023-10-15 00:48:10 -0400

    made chatbot feature

--- therapy_bot/src/App.js
+++ therapy_bot/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import Quiz from './Components/quiz';
 import Navbar from './Components/nav';
+import Chat from './Components/chat'
 import Journals from './Components/journals';
 //import Login from './Components/Pages/login';
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path = "/" element={<Home/>}></Route>
           <Route path = "/journal" element={<Journals/>}></Route>
+          <Route path = "/chat" element={<Chat/>}></Route>
           <Route path = "/quiz" element={<Quiz/>}></Route>
         </Routes>
       </Router>
