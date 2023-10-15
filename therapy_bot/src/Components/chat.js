9747464610fce7b388570dfc8a12b5f1250f2c99
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2023-10-15 01:24:24 -0400

    chat

--- therapy_bot/src/Components/chat.js
+++ therapy_bot/src/Components/chat.js
@@ -3,7 +3,7 @@ import './chat.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
